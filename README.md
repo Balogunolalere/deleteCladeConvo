abdd09f40ecec3d188b5c58d1f72846b65159d43
Balogun Olalere <lordareello@gmail.com>
2023-07-25 02:12:57 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,68 @@
+## Claude Conversation Manager
+
+### This is a simple command line application for managing conversations in Claude.
+### Prerequisites
+
+    Python 3.6+
+    Claude account
+    Claude session key (SESSION_KEY)
+    Claude account id (ACCOUNT_ID)
+
+### Installation
+
+    Clone the repo
+
+
+### Install dependencies
+    
+    pip install -r requirements.txt
+
+    Set the SESSION_KEY and ACCOUNT_ID environment variables
+
+### Usage
+
+
+$ python manager.py --help
+
+Usage: manager.py [OPTIONS] COMMAND [ARGS]...
+
+  Options:
+
+    --install-completion  Install completion for the current shell.
+    --show-completion     Show completion for the current shell, to copy it or
+                          customize the installation.
+
+    --help                Show this message and exit.
+
+  Commands:
+
+    delete-conversation  Delete a conversation by UUID.
+    delete-conversations  Delete all conversations. 
+    fetch-conversations   Fetch all conversations.
+
+Fetch conversations
+
+
+$ python manager.py fetch-conversations
+
+This will make a request to fetch all conversations and print them to the console.
+Delete a conversation
+
+
+$ python manager.py delete-conversation [uuid]
+
+Delete a conversation by its UUID.
+Delete all conversations
+
+
+$ python manager.py delete-conversations
+
+Loop through and delete all conversations.
+Environment Variables
+
+    SESSION_KEY - Claude session key
+    ACCOUNT_ID - Claude account ID
+
+### Contributing
+
+Pull requests are welcome. Feel free to open an issue first to discuss what you would like to change.
---
+++ app.py
@@ -0,0 +1,88 @@
+import re
+import typer
+import requests
+from time import sleep
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = typer.Typer()
+
+SESSION_KEY = os.getenv("SESSION_KEY")
+ACCOUNT_ID = os.getenv("ACCOUNT_ID")
+BASE_URL = f"https://claude.ai/api/organizations/{ACCOUNT_ID}/chat_conversations" 
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0",
+    "Accept": "*/*",
+    "Accept-Language": "en-US,en;q=0.5",
+    "Referer": "https://claude.ai/chats",
+    "anthropic-client-version": "1",
+    "Content-Type": "application/json",
+    "Connection": "keep-alive",
+}
+
+@app.command()
+def fetch_conversations():
+    response = requests.get(
+        BASE_URL, 
+        cookies={"sessionKey": SESSION_KEY},
+        headers=HEADERS
+    )
+    
+    conversations = [
+        {"name": c["name"], "uuid": c["uuid"], "created_at": c["created_at"]} 
+        for c in response.json()
+    ]
+    
+    typer.echo("\n*** fetching conversations ***")
+    typer.echo(conversations)
+
+@app.command()
+def delete_conversation(uuid: str):
+    if not re.match(r"^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$", uuid):
+        typer.echo("Invalid uuid format.")
+        raise typer.Exit()
+        
+    delete_url = f"{BASE_URL}/{uuid}"
+    response = requests.delete(
+        delete_url, 
+        cookies={"sessionKey": SESSION_KEY},
+        headers=HEADERS
+    )
+    
+    if response.status_code == 204:
+        typer.echo(f"Conversation {uuid} deleted successfully!")
+    else:
+        typer.echo(f"Failed to delete conversation {uuid}")
+        
+@app.command()        
+def delete_conversations():
+    response = requests.get(
+        BASE_URL,
+        cookies={"sessionKey": SESSION_KEY},
+        headers=HEADERS
+    )
+    
+    for conversation in response.json():
+        sleep(3)
+        
+        conversation_id = conversation["uuid"]
+        delete_url = f"{BASE_URL}/{conversation_id}"
+        
+        delete_response = requests.delete(
+            delete_url,
+            cookies={"sessionKey": SESSION_KEY}, 
+            headers=HEADERS
+        )
+        
+        typer.echo("\n*** deleting conversation ***")
+        sleep(3)
+        
+        typer.echo(f"deleted conversation: {conversation_id}")
+        
+    typer.echo("Done!")
+    
+if __name__ == "__main__":
+    app()
